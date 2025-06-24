d4dd7522206036a7bdb275979f587616ea84559e
Therese Nevander <thne1900@student.miun.se>
2025-06-24 13:38:08 +0200

    Skapade element (index.html) och skrev ts-kod i filen (main.ts) för kurslistan. Styladet lite och skrev i md-filen.

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # TypeScript: Moment 1.2
 
 - Började med att starta upp ett Vite-projekt med kommando.
-- Kontrollerade att projektet kunde köras och visas i webbläsaren.  
-- Därefter utformades index.html dokumentet med webbplatens element och innehåll.
+- Kontrollerade att projektet kunde köras och visas i webbläsaren. 
+- Synkroniserade projektet till Netlify för publicering.  
+- Därefter utformades html-dokumentet med webbplatens element och innehåll.
+- Styling med utformning och placering gjordes i style.css
+- Typescript-kod gjordes i filen main.ts för att få ut kurslistan till webbplatsen. 
 
 
 
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,96 @@
 import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+//Kursobjekt med typer
+interface CourseInfo{
+  code:string;
+  coursename:string;
+  progression: string;
+  syllabus:string;
+}
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+//En array som lagrar kursdata:
+let coursesData:CourseInfo[]=[];
+
+//Vid sidinladdning: försöker ladda från localStorage, annars från API.
+window.onload=()=>{
+loadCoursesLocalStorage()||
+  getCourses();
+}
+
+//Funktion för lokalstorage:
+function loadCoursesLocalStorage():boolean{
+  const dataStorage=localStorage.getItem("coursesData");
+  if (dataStorage) {
+    try {
+      const parsedData:CourseInfo[]=JSON.parse(dataStorage);
+      coursesData=parsedData;
+
+      displayCoursesList();
+
+      console.log("Kursdatan hämtades från LocalStorage");
+      return true;
+    }catch(error) {
+      console.error("Inhämtningen från localstorage misslyckades", error);
+      return false;
+    }
+    }
+    return false;
+  }
+
+//Asynkron funktion för att hämta kurser via API. 
+async function getCourses(): 
+Promise<void>{
+  const url="https://webbutveckling.miun.se/files/ramschema_ht24.json";
+
+  try {
+    const response=await fetch(url);
+    if(!response.ok) {
+      throw new Error("Fel vid inhämtningen av kursinformationen");
+    }
+    const data:CourseInfo[]=await response.json();
+    coursesData=data;
+
+    displayCoursesList();
+
+    localStorage.setItem("coursesData", JSON.stringify(data));
+
+    data.forEach(course=>{
+      console.log(`${course.coursename}`);
+
+    });
+  }catch (error){
+    console.error('Ett fel har inträffat:', error);
+  }
+}
+
+//Funktion för att få ut kurserna, DOM
+function displayCoursesList(){
+  const courseList:HTMLElement|null=document.getElementById("coursesInfoList");
+  if(!courseList){
+    console.error("Elementet för listan hittades inte");
+    return;
+  }
+  
+ courseList.innerHTML="";
+
+ coursesData.forEach(course=>{
+  const li=document.createElement("li");
+
+let links=document.createElement("a");
+links.href=course.syllabus;
+links.innerHTML=`${course.syllabus}`;
+
+  li.innerHTML=`<strong>KURSKOD:</strong> ${course.code} <strong>KURSNAMN:</strong> 
+  <i>${course.coursename}</I> <strong>PROGRESSION:</strong> ${course.progression} <strong>KURSLÄNK:</strong>`;
+  
+  li.appendChild(links);
+  courseList.appendChild(li);
+
+  li.style.margin="4%";
+
+ });
+ 
+}
+
+
 
